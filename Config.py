5460af6d06bdf2cad9025e1a732dc5b03f84f6e0
Pinogpog <102848360+Pinogpog@users.noreply.github.com>
2022-08-28 15:27:07 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("APP_ID", "9688162"))
+    API_HASH = os.environ.get("API_HASH", "7b2bd6db5aaf2fc6c21d08249189c028")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5621197727:AAGUdK7AErXR-j2UC-p_8RIa_euprsSNf2U")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Zaid2_Robot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Jamjamxmusicbot")
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
